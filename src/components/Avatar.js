3c8dea5ff6220151e18d243d6dd90101da3c4005
Jzolate <nuthapong.porn@bumail.net>
2022-02-26 16:56:44 +0700

    class/07/ex01 for "1630701918"

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -5,7 +5,18 @@ function Avatar(props) {
     <div className="avatar">
       <img src={`https://avatars.dicebear.com/api/human/${props.name}.svg`} alt={props.name}/>
     </div>
+    
   );
 }
 
 export default Avatar;
+
+/*
+data.map((item,index) => {
+  return (<div key={index} className={`friendlist-item ${item.position}`}>
+    <div>{item.user}</div>
+    <Avatar name={item.user}/>
+    <div className="displayName"> {item.displayName}</div>
+  </div>)
+})
+*/
